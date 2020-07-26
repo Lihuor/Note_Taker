31bfd0ba90eeec2bef89445ace13bd9d9ede6c24
Lihuor <lihuor@gmail.com>
2020-07-26 14:58:22 +1000

    Fixed comments

--- server.js
+++ server.js
@@ -10,57 +10,40 @@ const PORT = process.env.PORT || 8080;
 //  Initialize notesData
 let notesData = [];
 
-// Set up body parsing, static, and route middleware
+// Set up the Express app to handle data parsing
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static(path.join(__dirname, "Develop/public")));
 
 // ROUTES
 // ============================================================================
-
-// api call response for all the notes, and sends the results to the browser as an array of object
-
 app.get("/api/notes", function(err, res) {
   try {
-    // reads the notes from json file
+    
     notesData = fs.readFileSync("Develop/db/db.json", "utf8");
-    // console.log("hello!");
-    // parse it so notesData is an array of objects
     notesData = JSON.parse(notesData);
 
-    // error handling
   } catch (err) {
     console.log("\n error (in app.get.catch):");
     console.log(err);
   }
-  //   send objects to the browser
   res.json(notesData);
 });
 
 // writes the new note to the json file
 app.post("/api/notes", function(req, res) {
   try {
-    // reads the json file
     notesData = fs.readFileSync("./Develop/db/db.json", "utf8");
     console.log(notesData);
-
-    // parse the data to get an array of objects
     notesData = JSON.parse(notesData);
-    // Set new notes id
     req.body.id = notesData.length;
-    // add the new note to the array of note objects
-    notesData.push(req.body); // req.body - user input
-    // make it string(stringify)so you can write it to the file
+    notesData.push(req.body);
     notesData = JSON.stringify(notesData);
-    // writes the new note to file
     fs.writeFile("./Develop/db/db.json", notesData, "utf8", function(err) {
-      // error handling
       if (err) throw err;
     });
-    // changeit back to an array of objects & send it back to the browser(client)
     res.json(JSON.parse(notesData));
 
-    // error Handling
   } catch (err) {
     throw err;
     console.error(err);
@@ -71,26 +54,18 @@ app.post("/api/notes", function(req, res) {
 
 app.delete("/api/notes/:id", function(req, res) {
   try {
-    //  reads the json file
     notesData = fs.readFileSync("./Develop/db/db.json", "utf8");
-    // parse the data to get an array of the objects
     notesData = JSON.parse(notesData);
-    // delete the old note from the array on note objects
     notesData = notesData.filter(function(note) {
       return note.id != req.params.id;
     });
-    // make it string(stringify)so you can write it to the file
     notesData = JSON.stringify(notesData);
-    // write the new notes to the file
     fs.writeFile("./Develop/db/db.json", notesData, "utf8", function(err) {
-      // error handling
       if (err) throw err;
     });
 
-    // change it back to an array of objects & send it back to the browser (client)
     res.send(JSON.parse(notesData));
 
-    // error handling
   } catch (err) {
     throw err;
     console.log(err);
@@ -98,13 +73,11 @@ app.delete("/api/notes/:id", function(req, res) {
 });
 
 // HTML GET Requests
-
 // Web page when the Get started button is clicked
 app.get("/notes", function(req, res) {
   res.sendFile(path.join(__dirname, "Develop/public/notes.html"));
 });
 
-// If no matching route is found default to home
 app.get("*", function(req, res) {
   res.sendFile(path.join(__dirname, "Develop/public/index.html"));
 });
